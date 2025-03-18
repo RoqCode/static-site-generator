3e41ae535ec1962bf6601cbcad3a9829f7b952ee
Hajo Haas <hajo.haas@hmmh.de>
2025-03-18 15:19:18 +0100

    L1: fix eq method

--- src/textnode.py
+++ src/textnode.py
@@ -16,10 +16,14 @@ class TextNode:
         self.text_type = text_type
         self.url = url
 
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
 
-def __eq__(text_node_1, text_node_2):
-    return text_node_1 == text_node_2
-
-
-def __repr__(text, text_type, url):
-    return f"TextNode({text}, {text_type.value}, {url}"
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
